2c8bface3c709d88ae42877461dcee240002d4cc
jonnycs <jonnydevvs@gmail.com>
2026-03-01 19:17:24 -0800

    Add and style header section

--- webpack.config.js
+++ webpack.config.js
@@ -17,4 +17,16 @@ export default {
       template: "./src/template.html",
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html$/i,
+        use: ["html-loader"],
+      },
+    ],
+  },
 };
